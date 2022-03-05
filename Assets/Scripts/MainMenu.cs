cdfcb9c1b8305a718e3a86ab60b9f783134f5078
emmaflt <emmafouillat@gmail.com>
2022-03-05 11:39:27 +0100

    ajout du projet

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SettingsButton()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame");
+        Application.Quit();
+    }
+
+}
