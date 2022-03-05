5c16d85107983f995a1646f4df4dc0cd53e3c6b9
emmaflt <emmafouillat@gmail.com>
2022-03-05 12:54:46 +0100

    ajout du menu pause, pas entierement fonctionnel

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    [SerializeField] public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+
+            else
+            {
+                Paused();
+            }
+        }
+
+    }
+
+    void Paused()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        gameIsPaused = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        //DontDestroyOnLoadScene.instance.RemoveFromDontDestroyOnLoad();
+        SceneManager.LoadScene("MainMenu");
+    }
+}
