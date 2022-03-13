acea4c72c04c86555134ec12aab6c9bbbc50083c
0ur0boss <enzo.duret@yahoo.fr>
2022-03-13 18:58:50 +0100

    ajout du niveau 4, probleme quand on monte, je n est pas encore fais le doo & key

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public bool isOpen;
+    //public Animator animator;
+    public void OpenChest()
+    {
+        if (!isOpen)
+            isOpen = true;
+            Debug.Log("Chest is now open...");
+           // animator.SetBool("IsOpen", isOpen);
+    }
+}
