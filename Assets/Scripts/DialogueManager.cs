f4bca984b41d7e31101a9cf84b825b9c6d876719
emmaflt <emmafouillat@gmail.com>
2022-03-18 18:06:58 +0100

    pull

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public static DialogueManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de DialogueManager dans la scène");
+            return;
+        }
+        instance = this;
+    }
+}
